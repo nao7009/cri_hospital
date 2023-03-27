dde21b139e4a79fb19a3b7148bdf45cc0c2bb02f
shimojo <nao0909n@yahoo.co.jp>
2023-03-27 20:08:36 +0900

    vue.config.js修正

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 // vue.config.js
-
 module.exports = {
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './',
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "cri_hospital"   
+    }
+  },
   css: {
     loaderOptions: {
       scss: {
